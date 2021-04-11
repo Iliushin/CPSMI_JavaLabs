9df36ab1d20a411d899b9d0dd5493b5833f106bd
Iliushin <ilyushin.a.g@bk.ru>
2021-04-12 00:20:47 +0300

    Добавлено две лабы

---
+++ src/One_HelloWorld/Hello_world.java
@@ -0,0 +1,13 @@
+package One_HelloWorld;
+
+
+// Выводит на экран 100 раз фразу Hello world!
+
+public class Hello_world {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++){
+            System.out.println(i + ". Hello world!");
+        }
+    }
+
+}
