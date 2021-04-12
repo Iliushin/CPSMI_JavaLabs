a12a0fa6777d50855df6c4e3d411ec9b6097020d
Iliushin <ilyushin.a.g@bk.ru>
2021-04-12 13:18:02 +0300

    Третья лаба

---
+++ src/Three_DayOfWeek/DayOfWeek.java
@@ -0,0 +1,31 @@
+package Three_DayOfWeek;
+// По введенному числу в 'x' определяет день недели
+public class DayOfWeek {
+    public static void main(String[] args) {
+        int x = 17;
+        int i ;
+        switch (i = x%7){
+            case (1):
+                System.out.println("Понедельник");
+                break;
+            case (2):
+                System.out.println("Вторник");
+                break;
+            case (3):
+                System.out.println("Среда");
+                break;
+            case (4):
+                System.out.println("Четверг");
+                break;
+            case (5):
+                System.out.println("Пятница");
+                break;
+            case (6):
+                System.out.println("Суббота");
+                break;
+            case (7):
+                System.out.println("Воскресенье");
+                break;
+        }
+    }
+}
